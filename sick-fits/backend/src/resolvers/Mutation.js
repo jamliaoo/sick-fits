b74a94abeadbacaaddd4019c3ed856436f90847d
Jam Liao <jam.liao@bonio.com.tw>
2018-10-09 10:11:57 +0800

    init item api

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const { data } = args;
+    const item = await ctx.db.mutation.createItem({
+      data
+    }, info);
+    return item;
+  }
+};
 
 module.exports = Mutations;
