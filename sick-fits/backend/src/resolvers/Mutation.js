963cda67803f259dded8a002b287eea06b417312
mercer <skydreamfly@gmail.com>
2018-10-12 00:04:45 +0800

    init item method

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -5,6 +5,21 @@ const Mutations = {
       data
     }, info);
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    const { data, where } = args;
+    return ctx.db.mutation.updateItem({
+      data,
+      where,
+    }, info);
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    if (!item) return;
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
